7540c58e12f3c31196e30ce5ed36bc39ee9c1e95
Thomas Ferro <t.ferro184@gmail.com>
2020-07-27 10:19:22 +0200

    Core (front): clearing project files

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>My records collection</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
